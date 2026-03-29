2fe78d3d2ddccd6b74cdaa9c07a15eb46b0f970f
Guilherme Brito Andrade <guilhermebritoandrade17@gmail.com>
2026-03-29 12:10:34 -0300

    test: adicionando caso limite

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,11 +1,27 @@
-from src.core import calcular_proxima_revisao
+import sys
+import os
 from datetime import datetime
 import pytest
 
+# Faz o Python encontrar o src
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
+from src.core import calcular_proxima_revisao
+
+
+# ✅ 1. CAMINHO FELIZ
 def test_calculo_revisao_sucesso():
-    resultado = calcular_proxima_revisao(1)
+    resultado = calcular_proxima_revisao(3)
     assert isinstance(resultado, datetime)
 
-def test_erro_valor_negativo():
+
+# ❌ 2. ENTRADA INVÁLIDA
+def test_erro_nivel_invalido():
     with pytest.raises(ValueError):
         calcular_proxima_revisao(-1)
+
+
+# ⚠️ 3. CASO LIMITE
+def test_zero_dias():
+    resultado = calcular_proxima_revisao(0)
+    assert isinstance(resultado, datetime)
