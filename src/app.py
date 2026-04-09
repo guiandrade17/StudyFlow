2fc56f76c0598f4cc760a428d1fd8b91f08c32ea
Guilherme Brito Andrade <guilhermebritoandrade17@gmail.com>
2026-04-09 15:29:26 -0300

    fix: ajuste para rodar no render

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from src.core import calcular_proxima_revisao, obter_frase
+import os
 
 app = Flask(__name__)
 
@@ -16,4 +17,5 @@ def index():
     return render_template("index.html", resultado=resultado, frase=frase)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
